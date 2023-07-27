f465adf163cd13a4d7dd64a1e6b7155faca0cfc8
Altamash Zaheer <aaltamashzaheer@gmail.com>
2023-07-26 22:14:32 -0700

    Added whatsapp feature but not integrated

--- Student_Management_System/Actions.py
+++ Student_Management_System/Actions.py
@@ -87,7 +87,7 @@ def send_whatsapp_message(phone_number, message, hour, minute):
 # Usage example
 phone_number = "+923024691807"  # Replace with the recipient's phone number including the country code
 message = "Hello, this is a test message."
-hour = 22  # Replace with the desired hour (24-hour format)
-minute = 34  # Replace with the desired minute
+hour = 5  # Replace with the desired hour (24-hour format)
+minute = 37  # Replace with the desired minute
 
 send_whatsapp_message(phone_number, message, hour, minute)
