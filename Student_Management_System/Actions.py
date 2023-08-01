34ebdb0ab189e4d690668f227dedef54df8288b6
Altamash Zaheer <aaltamashzaheer@gmail.com>
2023-08-01 10:22:18 -0700

    updated

--- Student_Management_System/Actions.py
+++ Student_Management_System/Actions.py
@@ -5,8 +5,8 @@ def send_whatsapp_message(phone_number, message, hour, minute):
 
 # Usage example
 phone_number = "+923024691807"  # Replace with the recipient's phone number including the country code
-message = "Hello, this is a test message."
-hour = 5  # Replace with the desired hour (24-hour format)
-minute = 37  # Replace with the desired minute
+message = "Hello, this is a test message sent to me."
+hour = 10  # Replace with the desired hour (24-hour format)
+minute = 22  # Replace with the desired minute
 
 send_whatsapp_message(phone_number, message, hour, minute)
